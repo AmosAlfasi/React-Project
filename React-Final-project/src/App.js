d9592a6632efa0894cf2ab8d085997768f6c8119
AmosAlfasi <amos13389@gmail.com>
2022-09-24 19:23:31 +0300

    indexDb pull

--- React-Final-project/src/App.js
+++ React-Final-project/src/App.js
@@ -11,43 +11,46 @@ import AddCost from "./components/costs/AddCost";
 import useCostManager from "./services/useCostManager";
 import useOpenModal from "./services/useOpenModel";
 
-const initUsers = [
-  {
-    firstName: "diana",
-    lastName: "krakovich",
-    id: "0",
-    birthday: "1/1/200",
-    maritalSatus: "married",
-  },
-  {
-    firstName: "amos",
-    lastName: "alfasi",
-    id: "1",
-    birthday: "1/1/200",
-    maritalSatus: "married",
-  },
-  {
-    firstName: "Itay",
-    lastName: "Amini",
-    id: "2",
-    birthday: "1/1/200",
-    maritalSatus: "Open for ",
-  },
-  {
-    firstName: "amos",
-    lastName: "test",
-    id: "3",
-    birthday: "1/1/200",
-    maritalSatus: "married",
-  },
-  {
-    firstName: "shmuel",
-    lastName: "zibi",
-    id: "4",
-    birthday: "1/1/200",
-    maritalSatus: "married",
-  },
-];
+let initUsers = [];
+//  [
+//   {
+//     firstName: "diana",
+//     lastName: "krakovich",
+//     id: "0",
+//     birthday: "1/1/200",
+//     maritalSatus: "married",
+//   },
+//   {
+//     firstName: "amos",
+//     lastName: "alfasi",
+//     id: "1",
+//     birthday: "1/1/200",
+//     maritalSatus: "married",
+//   },
+//   {
+//     firstName: "Itay",
+//     lastName: "Amini",
+//     id: "2",
+//     birthday: "1/1/200",
+//     maritalSatus: "Open for ",
+//   },
+//   {
+//     firstName: "amos",
+//     lastName: "test",
+//     id: "3",
+//     birthday: "1/1/200",
+//     maritalSatus: "married",
+//   },
+//   {
+//     firstName: "shmuel",
+//     lastName: "zibi",
+//     id: "4",
+//     birthday: "1/1/200",
+//     maritalSatus: "married",
+//   },
+// ];
+
+// Itay's variables - to be deleted
 
 function App() {
   //creating our demo namespace
@@ -62,12 +65,7 @@ function App() {
     console.log("The web browser doesn't support IndexedDB");
   }
   //the data we want to store in our indexeddb database
-  demo.data = initUsers; /*[
-    { id: "1232523", brand: "Toyota", year: 2012, model: "Corola" },
-    { id: "2343434", brand: "Mazda", year: 2008, model: "6" },
-    { id: "2234345", brand: "Fiat", year: 2014, model: "500 Large" },
-    { id: "2234333", brand: "Fiat", year: 2011, model: "Bravo" },
-  ];*/
+  demo.data = initUsers; //initUsers;
 
   demo.db = null;
   demo.request = window.indexedDB.open("projectdb", 1);
@@ -89,6 +87,7 @@ function App() {
     var database = event.target.result;
     var usersStore = database.createObjectStore("users", { keyPath: "id" });
     var costsStore = database.createObjectStore("costs", { keyPath: "id" });
+
     for (var i in demo.data) {
       usersStore.add(demo.data[i]);
     }
@@ -119,7 +118,7 @@ function App() {
   }
 
   function readAllItems() {
-    var objectStore = demo.db.transaction("cars").objectStore("cars");
+    var objectStore = demo.db.transaction("users").objectStore("users");
 
     objectStore.openCursor().onsuccess = function (event) {
       var cursor = event.target.result;
@@ -137,6 +136,34 @@ function App() {
         cursor.continue();
       } else {
         console.log("readAllItems(): no more entries!");
+      }
+    };
+  }
+
+  function getAllUsers() {
+    var objectStore = demo.db.transaction("users").objectStore("users");
+    var allUsers = [];
+    objectStore.openCursor().onsuccess = function (event) {
+      var cursor = event.target.result;
+      var tempUser = {};
+      if (cursor) {
+        tempUser = {
+          firstName: cursor.value.firstName,
+          lastName: cursor.value.lastName,
+          id: cursor.value.id,
+          birthday: cursor.value.birthday,
+          maritalSatus: cursor.value.maritalSatus,
+        };
+
+        cursor.continue();
+        allUsers = [...allUsers, tempUser];
+      } else {
+        console.log("readAllItems(): no more entries!");
+
+        setUsers((prevValue) => {
+          console.log("bla");
+          return [[], ...allUsers];
+        });
       }
     };
   }
@@ -170,24 +197,26 @@ function App() {
       console.log("removeItem(): problem with removing a data item from the database");
     };
   }
+
   const [selectedUser, setSelectedUser] = useState(null);
 
   const { costs, addCost, removeCost, refreshCosts } = useCostManager(selectedUser?.id ?? null, {
     sortFn: (a, b) => b.timestamp - a.timestamp,
   });
+
   const [users, setUsers] = useState(initUsers);
   const [filteredUsers, setFilteredUsers] = useState(initUsers);
   const [userInfoVisible, setShowUserInfo, setHideUserInfo] = useOpenModal(false);
   const [addUserVisible, setShowAddUser, setHideAddUser] = useOpenModal(false);
   const [addCostVisible, setShowAddCost, setHideAddCost] = useOpenModal(false);
 
-  // useEffect(() => {
-  //   console.log("current costs: ", costs);
-  // }, [costs]);
-
   useEffect(() => {
     refreshCosts();
   }, [selectedUser]);
+
+  useEffect(() => {
+    setFilteredUsers(users);
+  }, [users]);
 
   const showInfoHandler = (user) => {
     setSelectedUser(user);
@@ -224,14 +253,15 @@ function App() {
     setHideAddUser();
 
     if (params.newUser) {
-      setUsers((prevValue) => {
-        return [...prevValue, params.newUser];
-      });
-      setFilteredUsers((prevValue) => {
-        return [...prevValue, params.newUser];
-      });
+      // setUsers((prevValue) => {
+      //   return [...prevValue, params.newUser];
+      // });
+      // setFilteredUsers((prevValue) => {
+      //   return [...prevValue, params.newUser];
+      // });
 
       addItem(params.newUser);
+      getAllUsers();
     }
   };
 
